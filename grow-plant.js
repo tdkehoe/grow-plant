b3095f373f7ba6b776f323d349404e10f7047b18
Thomas David Kehoe <kehoe@casafuturatech.com>
2015-06-01 09:53:53 -0600

    Removed semi-colons

--- grow-plant.js
+++ grow-plant.js
@@ -5,25 +5,25 @@ var myPlant = {
     flower: "doesn't have",
 
     care: function(type, water, food, antifreeze, sun) {
-      myPlant.typeOfPlant = type;
-      if (type === 'rose') {
-        if (water > 0) { myPlant.state = 'alive' };
-        if (water > 0) { myPlant.isPlant = true };
-        if (water > 0) { myPlant.height = myPlant.height + (2 * water) };
-        if (food > 0) { myPlant.height = myPlant.height + (1 * food) };
-        if (myPlant.height > 12) {myPlant.height = 12};
-        if (sun > 0) { myPlant.flower = "has" };;
-        if (antifreeze > 0) { myPlant.state = 'dead' };
-        if (water > 4 && food > 4) { myPlant.state = 'dead' };
-      }
+        myPlant.typeOfPlant = type;
+        if (type === 'rose') {
+            if (water > 0) { myPlant.state = 'alive' }
+            if (water > 0) { myPlant.isPlant = true }
+            if (water > 0) { myPlant.height = myPlant.height + (2 * water) }
+            if (food > 0) { myPlant.height = myPlant.height + (1 * food) }
+            if (myPlant.height > 12) {myPlant.height = 12}
+            if (sun > 0) { myPlant.flower = "has" }
+            if (antifreeze > 0) { myPlant.state = 'dead' }
+            if (water > 4 && food > 4) { myPlant.state = 'dead' }
+        }
 
-      if (type === 'pine') {
-        if (water > 0) { myPlant.state = 'alive' };
-        if (water > 0) { myPlant.isPlant = true };
-        if (water > 0) { myPlant.height = myPlant.height + (4 * water) };
-        if (food > 0) { myPlant.height = myPlant.height + (2 * food) };
-        if (antifreeze > 0) { myPlant.state = 'dead' };
-      }
+        if (type === 'pine') {
+            if (water > 0) { myPlant.state = 'alive' }
+            if (water > 0) { myPlant.isPlant = true }
+            if (water > 0) { myPlant.height = myPlant.height + (4 * water) }
+            if (food > 0) { myPlant.height = myPlant.height + (2 * food) }
+            if (antifreeze > 0) { myPlant.state = 'dead' }
+        }
     }
 };
 
